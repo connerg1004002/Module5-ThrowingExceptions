961db38509911a25ef8e703f9c4a980c3d842c2d
connerg1004002 <connerg@otc.edu>
2025-02-13 09:26:44 -0600

    Added exception tests

---
+++ Module5-TestingExceptions/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+[TestClass]
+public class Tests {
+  
+  //Username should throw error
+  [TestMethod]
+  public void CheckIfValidateThrowsErrorOnBadUsername() {
+    Assert.ThrowsException<InvalidUsernameException>
+      (() => Validate.ValidateCredentials("f", "Password123!"));
+  }
+
+  //Password should throw error
+  [TestMethod]
+  public void CheckIfValidateThrowsErrorOnBadPassword() {
+    Assert.ThrowsException<InvalidPasswordException>
+      (() => Validate.ValidateCredentials("Username", "j"));
+  }
+
+  //No errors should be thrown
+  [TestMethod]
+  public void CheckIfValidateThrowsNoErrorOnCorrectInput() {
+    try {
+      Validate.ValidateCredentials("Username", "Password123!");
+    } catch(Exception) {
+      Assert.Fail("Threw Exception");
+    }
+  }
+}
